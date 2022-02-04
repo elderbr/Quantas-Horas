65e4a8604d9b69a70993d4fa7ce06158c73ea1e2
ElderBR <elderbarbosaromao@gmail.com>
2022-02-03 22:33:13 -0300

    Adcionando Menu para adicionar a hora máxima

---
+++ app/src/main/java/br/com/elderbr/android/quantashoras/Conexao.java
@@ -0,0 +1,2 @@
+package br.com.elderbr.android.quantashoras;public class Conexao {
+}
---
+++ app/src/main/java/br/com/elderbr/android/quantashoras/HorasActivity.java
@@ -0,0 +1,14 @@
+package br.com.elderbr.android.quantashoras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HorasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horas);
+    }
+}
